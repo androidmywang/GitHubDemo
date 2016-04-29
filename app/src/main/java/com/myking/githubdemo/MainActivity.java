0d7a290bafaa5cf992855dd70b53e258793b6fbd
myking <137070566@163.com>
2016-04-29 09:02:16 +0800

    测试 11

--- app/src/main/java/com/myking/githubdemo/MainActivity.java
+++ app/src/main/java/com/myking/githubdemo/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Hello");
     }
 }
